b1bff0e0bf30eabe3efc77da2efd511472aa5023
pranilchhetri <pranilchhetri2014@gmail.com>
2019-04-17 13:40:46 +0545

    all in one

--- navbar.php
+++ navbar.php
@@ -1,5 +1,55 @@
-<?php include 'header.php';
-?>
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html lang="en" class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html lang="en" class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html lang="en" class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
+    <head>
+        <!-- meta charec set -->
+        <meta charset="utf-8">
+        <!-- Always force latest IE rendering engine or request Chrome Frame -->
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <!-- Page Title -->
+        <title>Hotel Atlas</title>      
+        <!-- Meta Description -->
+        <meta name="description" content="Blue One Page Creative HTML5 Template">
+        <meta name="keywords" content="one page, single page, onepage, responsive, parallax, creative, business, html5, css3, css3 animation">
+        <meta name="author" content="Muhammad Morshed">
+        <!-- Mobile Specific Meta -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <!-- Google Font -->
+        
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
+
+        <!-- CSS
+        ================================================== -->
+        <!-- Fontawesome Icon font -->
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+        <!-- Twitter Bootstrap css -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <!-- jquery.fancybox  -->
+        <link rel="stylesheet" href="css/jquery.fancybox.css">
+        <!-- animate -->
+        <link rel="stylesheet" href="css/animate.css">
+        <!-- Main Stylesheet -->
+        <link rel="stylesheet" href="css/main.css">
+        <!-- media-queries -->
+        <link rel="stylesheet" href="css/media-queries.css">
+
+        <!-- Modernizer Script for old Browsers -->
+        <script src="js/modernizr-2.6.2.min.js"></script>
+
+    </head>
+    
+    <body id="body">
+    
+        <!-- preloader 
+        <div id="preloader">
+            <img src="img/preloader.gif" alt="Preloader">
+        </div>
+        <!-- end preloader -->
+
+
 <div id="fb-root"></div>
 <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v3.2&appId=2070973373222926&autoLogAppEvents=1"></script>
         <!-- 
